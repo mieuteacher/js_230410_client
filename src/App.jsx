11dc849031cc3c52093594a6f059fe869f3a3eac
Mieu Teacher <mieuteacher@gmail.com>
2023-08-01 17:10:02 +0700

    finish add redux, redux toolkit into project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+import {useState, useEffect} from 'react';
 import './main.scss'
+
+import {useSelector, useDispatch} from 'react-redux';
+import { userActions } from './stores/slices/user';
+
 function App() {
+  const dispatch = useDispatch();
+
+  const userStore = useSelector(store => store.userStore);
+
+  useEffect(() => {
+    dispatch(userActions.find());
+  }, [])
   return (
     <div className="App">
-      hello em la nhanh build ket hop a va c
+      <button onClick={() => {
+        dispatch(userActions.changeLoad())
+      }}>Change Load</button>
+      <ul>
+        {
+          userStore.data.map((user, index) => (
+            <li key={user.id}>
+              <div>
+                User Email: {user.email}
+              </div>
+              <img style={{width: "100px", height: "100px", borderRadius: "50%"}} 
+              src={`${process.env.REACT_APP_SERVER_HOST}${user.avatar}`}/>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
